2727436c49ebab67820171bc0ab8dc5102b26de2
helgigretargunnars.96@gmail.com <helgigretargunnars.96@gmail.com>
2018-03-27 22:12:35 +0000

    setti upp forsíðuna en engir linkar tengdir

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class Home extends Component {
-
+  state = {isLoggedIn:true};
   render() {
-
     /* todo birta mismunandi upplýsingar ef innskráður notandi eða ekki */
-
+    const visible = this.state.isLoggedIn ?
+    <div className ="IsLoggedIn">
+      <p>Þú ert skráður notandi og getur því skráð bækur og breytt þeim sem til eru</p>
+      <p> Einnig geturu skoðað aðra notendur </p>
+    </div>:
+    <div className="isNotLoggedIn">
+      <p> Til að njóta bókasafnsins til fullnustu mælum
+      við með að skrá sig inn. Þangað til getur þú skoðað allar bækurnar.</p>
+    </div>
     return (
       <div>
-        <p>Halló heimur!</p>
-        <p><Link to="/login">Innskráning</Link></p>
+      <h2> Velkominn á bókasafnið</h2>
+        {visible}
       </div>
     );
   }
