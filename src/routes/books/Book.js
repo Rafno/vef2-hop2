c1e608a855e2a4c6578c2021b65d279424117143
helgigretargunnars.96@gmail.com <helgigretargunnars.96@gmail.com>
2018-03-28 13:57:32 +0000

    Get books komið en ekki buið að tengja linkinn

--- src/routes/books/Book.js
+++ src/routes/books/Book.js
@@ -1,19 +1,66 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { Route, Link, Switch } from 'react-router-dom';
 
-import './books.css';
+
+
+/**
+ * Í þessum component ætti að vera mest um að vera og séð um að:
+ * - Sækja gögn fyrir svið og birta
+ * - Opna/loka deildum
+ */
 
 class books extends Component {
-
-  render() {
+  state = { data: null, loading: true, error: false, linkur: null, gogn:null, checked:'', confirm:'+', visibleschool:null}
+  async componentDidMount() {
+    try {
+      const linkur ='https://vefforritun2-h1-synilausn.herokuapp.com/books';
+      let data = await this.fetchData(linkur);
+      this.setState({data, loading:false});
+    } catch (e) {
+      console.error('Error fetching navigation', e);
+      this.setState({ error: true, loading: false});
+    }
+  }
+  async fetchData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+  render(){
+  const { data, loading, error, gogn } = this.state;
+  if (loading) {
     return (
       <div>
-        <p>Innskráning</p>
+        <p>Sæki gögn...</p>
       </div>
     );
   }
+
+  if (error) {
+    return (
+      <div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="listForTitle">
+  {data.items.map((i,index) => {
+    return (
+      <ul>
+        <li>{i.title}</li>
+        <li>{i.author}</li>
+      </ul>
+    );
+  })
+  }
+  </div>
+ );
+
 }
+}
+export default books;
 
-/* todo tengja við redux */
 
-export default Book;
