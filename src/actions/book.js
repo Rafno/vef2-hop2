2f1c23f04f8fa97ab155632a83b905df77855ca3
Rafno <rafnar1993@gmail.com>
2018-03-28 17:09:22 +0000

    STOPP:100:

--- src/actions/book.js
+++ src/actions/book.js
@@ -3,3 +3,41 @@ import api from '../api';
 export const BOOK_REQUEST = 'BOOK_REQUEST';
 export const BOOK_SUCCESS = 'BOOK_SUCCESS';
 export const BOOK_FAILURE = 'BOOK_FAILURE';
+// uppfæra, bæta við lestri, eyða lestri.
+function requestBook() {
+  return {
+    type: BOOK_REQUEST,
+    isFetching: true,
+    message: null,
+  }
+}
+function receiveBook(id) {
+  return {
+    type: BOOK_SUCCESS,
+    isFetching: false,
+    id,
+    message: null,
+  }
+}
+
+function BookError(message) {
+  return {
+    type: BOOK_FAILURE,
+    isFetching: false,
+    message
+  }
+}
+// Thunk!
+
+export const callBook = (bookId) => {
+  return async (dispatch) => {
+    dispatch(requestBook());
+
+    let req;
+    try {
+      req = await api.get(bookId);
+    } catch (e) {
+      return dispatch(bookError(e))
+    }
+  }
+}
---
+++ src/reducers/Book.js
@@ -0,0 +1,5 @@
+import { BOOK_REQUEST, BOOK_SUCCESS, BOOK_FAILURE } from '../actions/Book';
+
+const initialState = {
+  isFetching: false,
+};
