37be2946861faf3bd364df4f5de4652e45854168
Rafno <rafnar1993@gmail.com>
2018-04-05 19:10:29 +0000

    TENGING KOMIN

--- src/actions/book.js
+++ src/actions/book.js
@@ -2,11 +2,9 @@ import api from '../api';
 import { FETCH_BOOKS, NEW_BOOK } from './types';
 
 export const fetchBooks = () => dispatch => {
-  const {
-    REACT_APP_SERVICE_URL: url = 'http://127.0.0.1:3002/',
-  } = process.env;
-  console.log(url);
-  fetch(`${url}books`)
+  const baseUrl = 'http://127.0.0.1:3002/';
+  console.log(process.env);
+  fetch(`${baseUrl}books`)
     .then(res => res.json())
     .then(books =>
       dispatch({
