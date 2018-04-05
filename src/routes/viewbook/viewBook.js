93980ec1cf8dbfba26bffe4e360b50576c133ef0
helgigretargunnars.96@gmail.com <helgigretargunnars.96@gmail.com>
2018-04-05 16:57:32 +0000

    byrjaður að vinna með eina bók api að koma líka

--- src/routes/viewbook/viewBook.js
+++ src/routes/viewbook/viewBook.js
@@ -6,13 +6,30 @@ import { Route, Link, Switch } from 'react-router-dom';
 
 
 /**
- * Hér mun þessi compnent sjá um alla listann af bókunum og getur valið spes bók.
+ * Get skoðað eina bók
  */
 
 class viewBook extends Component {
-
   render(){
-      return (<p> hello</p>);
+    const files = {
+       items: {"id":10,"title":"195","author":"Geore Cow","description":"Rosalega skemmtilega bókinn sem er voða merkileg","isbn10":"451774934","isbn13":"9777451524935","category":2,"published":"","pagecount":"246","language":"en"},
+    }
+    const gogn = this.componentDidMount();
+    console.log(gogn.items.length);
+     /* return (
+        <div className="skodaBok">
+          <ul className="listinnfyirBok">
+            <li>{items.title} </li>
+            <li> {items.author.items[0].items[0].id}</li>
+            <li> {items.isbn13} </li>
+            <li>Alveg lost hvað fiction er semsagt category </li>
+            <li> {items.description}</li>
+            <li> {items.pagecount} Síður </li>
+            <li> Tungumál: {items.language} </li>
+          </ul>
+        </div>
+      );*/
+      return(<p> jdjdj</p>);
     }
   }
 export default viewBook;
