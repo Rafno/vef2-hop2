51faf60a2804f963d658126cd10578c459db7858
Rafno <rafnar1993@gmail.com>
2018-04-05 17:57:13 +0000

    FOKKING HEROKU

--- src/reducers/Book.js
+++ src/reducers/Book.js
@@ -2,13 +2,13 @@ import { FETCH_BOOKS, NEW_BOOK } from '../actions/types';
 
 const initialState = {
   items: [],
-  item: {}
-
 };
 
 export default function (state = initialState, action) {
+  console.log(action.payload);
   switch (action.type) {
     case FETCH_BOOKS:
+    console.log('reducing');
       return {
         ...state,
         items: action.payload
