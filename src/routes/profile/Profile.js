c5561676df64557a4dacc9938b1ee5b4d72cdd5f
hinriksnaer <hinriksnaer@gmail.com>
2018-04-08 17:26:54 +0000

    added profile base

--- src/routes/profile/Profile.js
+++ src/routes/profile/Profile.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 
 export default class Profile extends Component {
 
+  handlePassChange = (e) => {
+    e.preventDefault();
+    const { pass, confirmPass } = this.state;
+    console.log(pass===confirmPass);
+  }
+
+  handleInputChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState( { [name]:value });
+  }
+
   render() {
     return (
       <div>
-        <p>Notendasíða</p>
+        <form>
+          <input type="file" name="pic" accept="image/*"/>
+          <input type="submit"/>
+        </form>
+        <form onSubmit={this.handlePassChange}>
+          <input type="password" name="pass" onChange={this.handleInputChange}/>
+          <input type="password" name="confirmPass" onChange={this.handleInputChange}/>
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
