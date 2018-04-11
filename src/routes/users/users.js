29cb51bb1d34d45426e97218bf45b2667b75d593
Rafno <rafnar1993@gmail.com>
2018-04-11 15:22:54 +0000

    Byrjað á users.

--- src/routes/users/users.js
+++ src/routes/users/users.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { CreateBook } from '../../actions/auth';
-import { UpdateBookById } from '../../actions/auth';
+import { viewUser } from '../../actions/auth';
 import { connect } from 'react-redux';
 
 class users extends Component {
-  render() {
+  componentDidMount() {
+    const { token } = this.props;
+    this.props.viewUser(token);
 
+  }
+  render() {
+    const { users } = this.props;
+    try {
+      console.log(users.response);
+
+    } catch (e) {
+      console.log();
+    }
     return (
       <p> hello </p>
     );
   }
 }
-export default users;
+const mapStateToProps = (state) => {
+  return {
+    isFetching: state.auth.isFetching,
+    isAuthenticated: state.auth.isAuthenticated,
+    users: state.auth.users,
+    token: state.auth.token,
+  }
+}
+
+export default connect(mapStateToProps, { viewUser })(users);
