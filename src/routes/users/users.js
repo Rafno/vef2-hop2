b8206c22b08a2b39b7b7cc9dcb8af556a008d124
helgigretargunnars.96@gmail.com <helgigretargunnars.96@gmail.com>
2018-04-12 18:32:45 +0000

    push ?

--- src/routes/users/users.js
+++ src/routes/users/users.js
@@ -5,18 +5,77 @@ import { viewUser } from '../../actions/auth';
 import { connect } from 'react-redux';
 
 class users extends Component {
+  state = {page:0, pageNumber:1};
   componentDidMount() {
     const { token } = this.props;
-    this.props.viewUser(token);
+    const { page, pageNumber } = this.state;
+    const url = this.props.location.search;
+    console.log(url);
+    const string = url.split('=');
+    console.log(string.length, " þetta er lengdin");
+    
+    if(string.length === 1) {
+      console.log('her er stuttur');
+    }else{
+      console.log("her er langur");
+    }
+    const givenNumber = string;
+    this.props.viewUser(token,page);
+    this.setState({page:page, pageNumber:pageNumber})
+  }
+  forwardHandler = (e) => {
+    e.preventDefault();
+    const { page, pageNumber } = this.state;
+    const numb = page+10;
+    const sida = pageNumber+1;
+    this.updateFall(numb, sida)
+  }
+  backwardHandler = (e) => {
+    e.preventDefault();
+    const { page, pageNumber } = this.state;
+    const numb = page-10;
+    const sida = pageNumber-1;
+    this.updateFall(numb, sida)
+  }
+  updateFall(tala,sida){
+    const { token } = this.props;
+    this.props.viewUser(token,tala);
+    this.setState({page:tala, pageNumber:sida})
 
   }
   render() {
+    const {page, pageNumber} = this.state;
     const { users } = this.props;
     console.log(users);
-    const userList = users ? 
-      <p>{users.response.items[0].name}</p> : <p>Hleð inn gögnum</p>
+    let next = null;
+    let prev = null;
+    const userList = users ?
+    <div>
+      {
+        users.response.items.map((i,index) => {
+          if( index == 0) {
+            prev = users.response.links.prev;
+            next = users.response.links.next;
+          }
+          return(
+          <ul>
+            <li> {i.name} </li>
+          </ul>
+          );
+        })
+      }
+    </div>
+      : <p>Hleð inn gögnum</p>
     return (
-      <div>{userList}</div>
+      <div>
+        <h2> Notendur </h2>
+        {userList}
+        <div className="takkar">
+          {prev ? <button className="backButton" onClick={this.backwardHandler}>Aftur um síðu</button> : null}
+          <p>Síða {pageNumber}</p>
+          {next ? <button className="forwardButton" onClick={this.forwardHandler}>Áfram um síðu </button> : null}
+        </div>
+      </div>
     );
   }
 }
